57c4334e2364aade7ed5b09e2ca69c5b514bc1a3
asiek <asiek@ZBook.home>
2019-10-18 00:34:28 +0200

    Finding number in array.

---
+++ Java exercices/src/Sevenheven2.java
@@ -0,0 +1,25 @@
+
+
+public class Sevenheven2 {
+public boolean findNumber( int[] number) {
+for (int i = 0; i < number.length; i++) {
+if (number[i] >= 10 || number[i]>= -10) {
+String num = String.valueOf(number[i]);
+char[] tab = num.toCharArray();
+for (int j = 0; j < tab.length ; j++) {
+if(tab[j] == '7') {
+return true;
+}
+}
+} else if (number[i] == 7) {
+return true;
+}
+}
+return false;
+}
+public static void main(String[] args) {
+int array[] = {5, 4, 3, 2, 0,1,23,58,7};
+Sevenheven2  obj = new Sevenheven2 ();
+System.out.println(obj.findNumber(array));
+}
+}
