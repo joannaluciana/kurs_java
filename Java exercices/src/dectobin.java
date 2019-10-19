c2811db8ebec87291dca5d6b50dbfbcf3dc1ea63
asiek <asiek@ZBook>
2019-10-19 23:32:11 +0200

    finally code

--- Java exercices/src/dectobin.java
+++ Java exercices/src/dectobin.java
@@ -1,4 +1,11 @@
 
 public class dectobin {
+   public static void main(String[] args) {
+	        int dec = 64;
 
-}
+	        String binary = Integer.toBinaryString(dec);
+	        System.out.print (dec +" is equal to " );
+	        System.out.print ("0b"+binary);
+	    }
+	}
+
