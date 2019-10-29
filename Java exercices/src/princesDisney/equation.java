dca6c838fcf791258ceef96b52c8fb0a1532d9af
asiek <asiek@192.168.0.53>
2019-10-29 15:25:11 +0100

    code with aquals method - it does not work :( proper

--- Java exercices/src/princesDisney/equation.java
+++ Java exercices/src/princesDisney/equation.java
@@ -2,28 +2,28 @@ package princesDisney;
 import java.util.*;
 public class equation {
 	
-	    public static void main(String[] args) {
+
+
+		public static void main(String[] args) {
 	        Scanner skan = new Scanner(System.in);
 	         System.out.println ("Pls give numbers to equation x = a +/- b");
 		         String streqsk = skan.nextLine();
-	            char[] streq = streqsk. toCharArray();
-               /* System.out.println (streq[0]);
-                System.out.println (Character.getNumericValue(streq[4]));
-                if      (streq[0]=='x'){System.out.println ("YES");}*/
-	            
-
-	           if       (streq[0]=='x' && streq [6]=='+'){System.out.println (Character.getNumericValue( streq[4])+Character.getNumericValue( streq[8])); }
-	            else if (streq[0]=='x' && streq [6]=='-'){System.out.println (Character.getNumericValue( streq[4])-Character.getNumericValue( streq[8])); }
-	            else if (streq[4]=='x' && streq [6]=='+'){System.out.println (Character.getNumericValue( streq[0])-Character.getNumericValue( streq[8])); }
-	            else if (streq[4]=='x' && streq [6]=='-'){System.out.println (Character.getNumericValue( streq[0])+Character.getNumericValue( streq[8])); }
-	            else if (streq[8]=='x' && streq [6]=='+'){System.out.println (Character.getNumericValue( streq[0])-Character.getNumericValue( streq[4])); }
-	            else if (streq[8]=='x' && streq [6]=='-'){System.out.println (Character.getNumericValue( streq[4])-Character.getNumericValue( streq[0])); }
-	            else if (streq[8]=='x' && streq [2]=='+'){System.out.println (Character.getNumericValue( streq[0])+Character.getNumericValue( streq[4])); }
-	            else if (streq[8]=='x' && streq [2]=='-'){System.out.println (Character.getNumericValue( streq[0])-Character.getNumericValue( streq[4])); }
-	            else if (streq[4]=='x' && streq [2]=='+'){System.out.println (Character.getNumericValue( streq[8])-Character.getNumericValue( streq[0])); }
-	            else if (streq[4]=='x' && streq [2]=='-'){System.out.println (Character.getNumericValue( streq[0])-Character.getNumericValue( streq[8])); }
-	            else if (streq[0]=='x' && streq [2]=='+'){System.out.println (Character.getNumericValue( streq[8])-Character.getNumericValue( streq[4])); }
-	            else if (streq[0]=='x' && streq [2]=='-'){System.out.println (Character.getNumericValue( streq[8])-Character.getNumericValue( streq[4])); }
+		         String [] streq = streqsk.split(" ");
+		         //System.out.println ((Integer.parseInt(streq[2]))+(Integer.parseInt(streq[4])));
+		     
+	          
+	       if       (streq[0].equals("x") && streq [3].equals("+")){System.out.println ((Integer.parseInt(streq[2]))+(Integer.parseInt(streq[4])));  }
+	           else if (streq[0].equals("x") && streq[3].equals("+")){System.out.println ((Integer.parseInt( streq[2]))-(Integer.parseInt( streq[4]))); }
+	           else if (streq[2].equals("x") && streq[3].equals("+")){System.out.println ((Integer.parseInt( streq[0])-Integer.parseInt( streq[4]))); }
+	            else if (streq[2].equals("x") && streq [3].equals("-")){System.out.println ((Integer.parseInt( streq[0])+Integer.parseInt( streq[4])));}
+	            else if (streq[4].equals("x") && streq [3].equals("+")){System.out.println ((Integer.parseInt( streq[0])-Integer.parseInt( streq[4]))); }
+	            else if (streq[4].equals("x") && streq [3].equals("-")){System.out.println ((Integer.parseInt( streq[4])-Integer.parseInt( streq[0]))); }
+	            else if (streq[4].equals("x") && streq [2].equals("+")){System.out.println ((Integer.parseInt( streq[0])+Integer.parseInt( streq[4]))); }
+	            else if (streq[4].equals("x") && streq [2].equals("+")){System.out.println ((Integer.parseInt( streq[0])-Integer.parseInt( streq[4]))); }
+	            else if (streq[3].equals("x") && streq [2].equals("+")){System.out.println ((Integer.parseInt( streq[4])-Integer.parseInt( streq[0]))); }
+	            else if (streq[3].equals("x") && streq [2].equals("-")){System.out.println ((Integer.parseInt( streq[0])-Integer.parseInt( streq[4])));}
+	            else if (streq[0].equals("x") && streq [2].equals("+")){System.out.println ((Integer.parseInt( streq[4])-Integer.parseInt( streq[2]))); }
+	            else  if (streq[0].equals("x") && streq [2].equals("-")){System.out.println ((Integer.parseInt( streq[4])-Integer.parseInt( streq[2])));}
 
 
 	/*x = a (+,-) b
